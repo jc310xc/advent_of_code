fdd621b514beaea207039d06e2da64336670fab8
Peter Mullins <petermullins@openarc.net>
2015-12-14 17:27:44 -0500

    Got rid of attempts to pack numbers, implement downto method to reveal binary representation of numbers

--- day7/code/some_assembly.rb
+++ day7/code/some_assembly.rb
@@ -6,7 +6,7 @@ def evalLHS(lhs, vals)
 	tokens[0].to_i
   elsif(tokens.length == 2)
     # Reduce a NOT
-	[~vals[tokens[1]].to_i].pack("n")
+	~vals[tokens[1]].to_i + 1
   else
     # We need to simplify further
     reduceStatement(tokens, vals)
@@ -28,19 +28,29 @@ def reduceStatement(tokens, values)
   end
   
   if(tokens[1] == "AND")
-    [operand1.to_i & operand2.to_i].pack("n")
+    operand1.to_i & operand2.to_i
   elsif(tokens[1] == "OR")
-    [operand1.to_i | operand2.to_i].pack("n")
+    operand1.to_i | operand2.to_i
   elsif(tokens[1] == "LSHIFT")
-    [operand1.to_i << operand2.to_i].pack("n")
+    operand1.to_i << operand2.to_i
   elsif(tokens[1] == "RSHIFT")
-    [operand1.to_i >> operand2.to_i].pack("n")
+    operand1.to_i >> operand2.to_i
   end
 end
 
 def printVals(vals)
   vals.each{|k,v|
-    puts k + ": " + v.to_s
+    print k + ": "
+
+    binNum = ""
+
+    16.downto(0) do |n|  binNum += v[n].to_s end
+
+    output = "0" + binNum[1..16]
+
+    print output.to_i(2)
+
+    print "\n"
   }
 end
 
@@ -50,9 +60,12 @@ file = open(filename)
 commands = file.readlines
 
 vals = Hash.new
-vals.default = [0].pack("n")
+vals.default = 0
+
+count = 1
 
 commands.each{ |cmd| 
+  puts "Evaluating command " + count.to_s + " of " + commands.count.to_s
   sides = cmd.split(" -> ")
   
   lhs = sides[0]
@@ -61,6 +74,7 @@ commands.each{ |cmd|
   rhs = rhs.tr("\n","")
   
   vals[rhs] = evalLHS(lhs, vals)
+  count += 1
 }
 
 printVals(vals)
