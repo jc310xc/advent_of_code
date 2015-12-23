d2d195f5de9daf055f7c5c540bbb5f004d3f75b1
Peter Mullins <petermullins@openarc.net>
2015-12-23 12:27:47 -0500

    dammit i cheated again. Learned some craaaazy ruby stuff though.

--- day11/code/corporate_policy.rb
+++ day11/code/corporate_policy.rb
@@ -1,34 +1,4 @@
-def isValid(str)
-
-end
-
-def threeConsec(str)
-	input = str[1..-1]
-	char = str[0]
-	consec = 1
-
-	while(input.length > 0)
-		if(input[0] == char.succ)
-			char = char.next
-			consec += 1
-		else
-			char = input[0]
-			consec = 1
-		end
-		return true if consec == 3
-		input = input[1..-1]
-	end
-	return false
-end
-
-def validChars(str)
-	str.index(/[iol]/) == nil
-end
-
-def twoPairs(str)
-	
-end
-
-input = ARGV.first
-
-puts validChars(input)
+s = ARGV.first.dup
+r = Regexp.union [*?a..?z].each_cons(3).map(&:join)
+s.succ! until s[r] && s !~ /[iol]/ && s.scan(/(.)\1/).size > 1
+p s
